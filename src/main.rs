1f9c9e5d8bed9b6b70b1f5bc3d32789bb7402f61
kanra824 <mdyh2a380824@gmail.com>
2021-03-07 02:30:15 +0900

    Move time definition

--- src/main.rs
+++ src/main.rs
@@ -352,7 +352,8 @@ impl<'a> State<'a> {
     }
 }
 
-fn init(state: &mut State, start: &Instant, time: u128) {
+fn init(state: &mut State, start: &Instant) {
+    let time = TIME_LIMIT / 300;
     let mut elapsed_time = start.elapsed().as_millis();
     while elapsed_time < time {
         let temperature: f64 = START_TMP + (END_TMP - START_TMP) * (elapsed_time as f64) / (TIME_LIMIT as f64);
@@ -428,7 +429,7 @@ fn main() {
     let seed = start.elapsed().as_nanos() as u64;
     let rand = Xorshift::with_seed(seed);
     let mut state = State::new(n, rand, &x, &y, &r);
-    init(&mut state, &start, TIME_LIMIT / 300);
+    init(&mut state, &start);
     simulate(&mut state, &start);
 
     let mul = 1000000000;
