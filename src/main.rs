082ac1bdf45e0be4adf5a7057da921b49989827e
kanra824 <mdyh2a380824@gmail.com>
2021-03-07 02:24:00 +0900

    yamanobori

--- src/main.rs
+++ src/main.rs
@@ -352,12 +352,12 @@ impl<'a> State<'a> {
     }
 }
 
-fn init(state: &mut State, start: &Instant) {
+fn init(state: &mut State, start: &Instant, time: u128) {
     let mut elapsed_time = start.elapsed().as_millis();
-    while elapsed_time < TIME_LIMIT / 5 {
+    while elapsed_time < time {
+        let temperature: f64 = START_TMP + (END_TMP - START_TMP) * (elapsed_time as f64) / (TIME_LIMIT as f64);
         for _ in 0..LOOP_PER_TIME_CHECK {
-            let temperature: f64 = START_TMP + (END_TMP - START_TMP) * (elapsed_time as f64) / (TIME_LIMIT as f64);
-            state.update(true, temperature, 10);
+            state.update(true, temperature, 100);
         }
         elapsed_time = start.elapsed().as_millis();
     }
@@ -366,12 +366,8 @@ fn init(state: &mut State, start: &Instant) {
 fn simulate(state: &mut State, start: &Instant) {
     let mut elapsed_time = start.elapsed().as_millis();
     while elapsed_time < TIME_LIMIT {
-        //eprintln!("{}", elapsed_time);
-        let saved_score = state.score;
-        let calculated_score = state.score_all();
-        //eprintln!("{} : {}", saved_score, calculated_score);
+        let temperature: f64 = START_TMP + (END_TMP - START_TMP) * (elapsed_time as f64) / (TIME_LIMIT as f64);
         for _ in 0..LOOP_PER_TIME_CHECK {
-            let temperature: f64 = START_TMP + (END_TMP - START_TMP) * (elapsed_time as f64) / (TIME_LIMIT as f64);
             state.update(true, temperature, 1);
         }
         elapsed_time = start.elapsed().as_millis();
@@ -432,7 +428,7 @@ fn main() {
     let seed = start.elapsed().as_nanos() as u64;
     let rand = Xorshift::with_seed(seed);
     let mut state = State::new(n, rand, &x, &y, &r);
-    init(&mut state, &start);
+    init(&mut state, &start, TIME_LIMIT / 300);
     simulate(&mut state, &start);
 
     let mul = 1000000000;
