b6b93777b98df654d6c57f5dc2723a64d777600c
kanra824 <mdyh2a380824@gmail.com>
2021-03-08 19:47:26 +0900

    Set maximize

--- opt.py
+++ opt.py
@@ -3,7 +3,7 @@ import subprocess
 
 def objective(trial):
     start_tmp = trial.suggest_uniform('start_tmp', 0, 0.1)
-    end_tmp = trial.suggest_uniform('end_tmp', 0, 0.1)
+    end_tmp = trial.suggest_uniform('end_tmp', start_tmp,  0.1)
     cmd = f'./all {start_tmp} {end_tmp}'
     print (cmd)
     child = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -11,7 +11,7 @@ def objective(trial):
     print(stdout, stderr)
     return int(stdout)
 
-study = optuna.create_study()
+study = optuna.create_study(direction='maximize')
 study.optimize(objective, n_trials=10)
 
 print(study.best_trial)
