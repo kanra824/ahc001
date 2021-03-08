44bec5f4b8e5ec5478649b48ff026741be3d9498
kanra824 <mdyh2a380824@gmail.com>
2021-03-08 19:25:02 +0900

    Parameter Tuning with optuna

---
+++ opt.py
@@ -0,0 +1,20 @@
+import optuna
+import subprocess
+
+def objective(trial):
+    start_tmp = trial.suggest_uniform('start_tmp', 0, 0.1)
+    end_tmp = trial.suggest_uniform('end_tmp', 0, 0.1)
+    cmd = f'./all {start_tmp} {end_tmp}'
+    print (cmd)
+    child = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = child.communicate()
+    print(stdout, stderr)
+    return int(stdout)
+
+study = optuna.create_study()
+study.optimize(objective, n_trials=10)
+
+print(study.best_trial)
+print()
+print (study.best_params)
+print (study.best_value)
